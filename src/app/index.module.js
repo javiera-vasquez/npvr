7a3c7c8df67c6e23ef5c97aaedd85ca3bac23dbc
Javier Enrique <s0wkete@gmail.com>
2016-03-23 17:36:26 -0300

    Flattering the services structure

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,34 +1,31 @@
-/* global malarkey:false, moment:false */
-
 // Settings
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
-import { initialConfig } from '../app/components/initialConfig.constant';
 // Controllers
 import { MainController } from './main/main.controller';
 import { ChannelController } from './channel/channel.controller';
 // Services
-// import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-// import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
+import { initialConfig } from '../app/services/initialConfig.constant';
+import { theMovieDB } from '../app/services/theMovieDB.service';
 // Directives
 import { windowHeight } from '../app/components/windowHeight.directive';
 import { bodyBackground } from '../app/components/bodyBackground.directive';
 
 // Import module
 angular.module('npvr', ['ngAnimate', 'ngSanitize', 'ngMessages', 'ui.router', 'toastr'])
-  .constant('malarkey', malarkey)
-  .constant('moment', moment)
   .constant('initialConfig', initialConfig)
   // Config and runtime of the app
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  // Main => services and controllers
-  .controller('MainController', MainController)
-  //.service('tmdbConfig', tmdbConfig)
-  //.service('tmdbNetwork', tmdbNetwork)
-  // Channel => services and controllers
-  .controller('ChannelController', ChannelController)
+  // Services
+  .factory('theMovieDB', theMovieDB)
+  // Directives
   .directive('windowHeight', windowHeight)
   .directive('bodyBackground', bodyBackground)
+  // Controllers
+  .controller('MainController', MainController)
+  .controller('ChannelController', ChannelController)
+
+
