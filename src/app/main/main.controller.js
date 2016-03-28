f136c1120bef184f0dac03cfbaaa1d3a1f92a641
Javier Enrique <s0wkete@gmail.com>
2016-03-28 18:31:42 -0300

    move the api config to main ctrl

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,12 +1,27 @@
 export class MainController {
 
-  constructor ($log, $window, initialConfig) {
+  constructor ($log, $window,theMovieDB, appConfig) {
     'ngInject'
 
     var mv = this;
-    mv.channelInfo = initialConfig();
 
-    $log.debug('Main Controller loadded');
+    mv.config = {};
+    mv.appConfig = appConfig();
+
+    theMovieDB.setParams({
+      api_key: mv.appConfig.key
+    });
+
+    var config = theMovieDB.getConfig(0);
+
+    config.then(
+      function(config) {
+        mv.config = config;
+      }, function(reason) {
+        $log.debug(reason);
+      })
+
+    $log.debug('Main Controller loadded', mv.appConfig);
   }
 
 }
