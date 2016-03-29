7b31217a845184ffbb9dc9a3c1f71190b0aa481c
Javier Enrique <s0wkete@gmail.com>
2016-03-29 15:03:07 -0300

    added timeline function to the shows

--- src/app/channel/channel.controller.js
+++ src/app/channel/channel.controller.js
@@ -11,7 +11,7 @@ export class ChannelController {
 
     theMovieDB.setParams({
       api_key: api.key,
-      with_networks: '64',
+      with_networks: '71',
       language: 'es'
     });
 
@@ -28,33 +28,38 @@ export class ChannelController {
     theMovieDB.getSeries(0)
       .then(
         (shows) => {
-          var id = 1;
+          // Init values for the loop
+          let id = 1;
           let showList = [];
           let len = shows.results.length;
+          // Time obj
           let time = {
+            now: new Date(),
+            date_stamp: parseTime(new Date()),
             day: 86400000, // 24h
-            lapsus: [5400000, 3600000, 1800000], // 90min, 60min, 30min
-            dateStamp: Date.parse(new Date()) - 43200000
+            lapsus: [5400000, 3600000, 1800000] // 90min, 60min, 30min
           };
+          // Loop adding random shows for 24 hours of time obj
           while (time.day > 0) {
-            let show = {};
             let num = _.random(0, (len - 1));
             let min = _.random(0, 2);
             // add info the de show
-            show.id = id;
-            show.now = false;
-            show.hour = time.dateStamp;
-            show.name = shows.results[num].name;
-            show.path = shows.results[num].poster_path;
-            show.overview = shows.results[num].overview
-            show.menu = ['Reiniciar programa', 'Ver más episodios', 'Grabar', 'Más info'];
+            let show = {
+              id: id,
+              imdb_id: shows.results[num].id,
+              time_line: setTime(time.now, time.date_stamp, time.lapsus[min]),
+              name: shows.results[num].name,
+              path: shows.results[num].poster_path,
+              overview: shows.results[num].overview,
+              menu: ['Reiniciar programa', 'Ver más episodios', 'Grabar', 'Más info']
+            };
             // push the program
             showList.push(show);
             // move the counter
             id++;
-            time.day = time.day - time.lapsus[min];
-            time.dateStamp = time.dateStamp + time.lapsus[min];
-            $log.debug(show);
+            time.day -=time.lapsus[min];
+            time.date_stamp += time.lapsus[min];
+            //$log.debug(show);
           }
           mv.shows = showList;
           $log.debug('newArray', showList);
@@ -66,4 +71,27 @@ export class ChannelController {
       });
   }
 
+}
+
+function parseTime(nowDate) {
+  let date = new Date(nowDate - (12*60*60*1000));
+  if(date.getMinutes() > 0 && date.getMinutes() < 16) {
+    date.setMinutes(0);
+  } else if (date.getMinutes() > 15 && date.getMinutes() < 46) {
+    date.setMinutes(30);
+  } else {
+    date.setMinutes(0);
+    date.setHours(date.getHours() + 1);
+  }
+  return Date.parse(date);
+}
+
+function setTime(nowDate, dateStamp, lapsus) {
+  let date = Date.parse(nowDate);
+  let obj = {
+    hour_start: dateStamp,
+    hour_end: dateStamp + lapsus
+  };
+  obj.is_live = (obj.hour_start < date && obj.hour_end > date) ? true : false;
+  return obj;
 }
